2f3d09cb0ba57fb9628597850e932bd7732be0fd
Edgar Ramírez Mondragón <edgarrm358@gmail.com>
2023-07-31 15:58:44 -0600

    Setup CI

---
+++ check_mssql.py
@@ -0,0 +1,57 @@
+import os
+
+from textwrap import dedent
+
+from sqlalchemy import create_engine, text
+
+MSSQL_ADDRESS = os.environ["MSSQL_ADDRESS"]
+MSSQL_PORT = os.environ["MSSQL_PORT"]
+MSSQL_USER = os.environ["MSSQL_USER"]
+MSSQL_PASSWORD = os.environ["MSSQL_PASSWORD"]
+MSSQL_DB = os.environ["MSSQL_DB"]
+
+URI = f"mssql+pymssql://{MSSQL_USER}:{MSSQL_PASSWORD}@{MSSQL_ADDRESS}:{MSSQL_PORT}/{MSSQL_DB}"
+
+engine = create_engine(URI)
+conn = engine.connect()
+
+# Create the table
+conn.execute(
+    text(
+        dedent(
+            """
+            CREATE TABLE IF NOT EXISTS test (
+                id INT IDENTITY(1,1) PRIMARY KEY,
+                name VARCHAR(50) NOT NULL
+            )
+            """,
+        ),
+    ),
+)
+
+# Insert some data
+conn.execute(
+    text(
+        dedent(
+            """
+            INSERT INTO test (name) VALUES ('foo')
+            INSERT INTO test (name) VALUES ('bar')
+            INSERT INTO test (name) VALUES ('baz')
+            """,
+        ),
+    ),
+)
+
+# Query the data
+result = conn.execute(
+    text(
+        dedent(
+            """
+            SELECT * FROM test
+            """,
+        ),
+    ),
+)
+
+for row in result:
+    print(row)
