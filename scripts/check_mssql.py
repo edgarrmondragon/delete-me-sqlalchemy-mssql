154b56d3ccc7ea163adcfdbdf02e2c5b75f477bd
Edgar Ramírez Mondragón <edgarrm358@gmail.com>
2023-07-31 17:16:02 -0600

    Support sqlalchemy 2

---
+++ scripts/check_mssql.py
@@ -0,0 +1,78 @@
+import os
+
+from textwrap import dedent
+
+from sqlalchemy import create_engine, text
+from sqlalchemy.engine import URL
+
+MSSQL_ADDRESS = os.environ["MSSQL_ADDRESS"]
+MSSQL_PORT = os.environ["MSSQL_PORT"]
+MSSQL_USER = os.environ["MSSQL_USER"]
+MSSQL_PASSWORD = os.environ["MSSQL_PASSWORD"]
+MSSQL_DB = os.environ["MSSQL_DB"]
+
+# URI = f"mssql+pymssql://{MSSQL_USER}:{MSSQL_PASSWORD}@{MSSQL_ADDRESS}:{MSSQL_PORT}/master"
+URI = URL.create(
+    "mssql+pymssql",
+    username=MSSQL_USER,
+    password=MSSQL_PASSWORD,
+    host=MSSQL_ADDRESS,
+    port=MSSQL_PORT,
+    database="master",
+)
+
+engine = create_engine(URI)
+
+# Test the connection
+print(f"Connecting to {URI}")
+# engine.execute(text("SELECT 1"))
+
+conn = engine.connect()
+
+print(f"Connected to {MSSQL_ADDRESS}:{MSSQL_PORT}")
+
+# Create the table
+with conn.begin():
+    conn.execute(
+        text(
+            dedent(
+                """
+                CREATE TABLE test (
+                    id INT IDENTITY(1,1) PRIMARY KEY,
+                    name VARCHAR(50) NOT NULL
+                )
+                """,
+            ),
+        ),
+    )
+
+    print("Created table 'test'")
+
+    # Insert some data
+    conn.execute(
+        text(
+            dedent(
+                """
+                INSERT INTO test (name) VALUES ('foo')
+                INSERT INTO test (name) VALUES ('bar')
+                INSERT INTO test (name) VALUES ('baz')
+                """,
+            ),
+        ),
+    )
+
+    print("Inserted data into 'test'")
+
+    # Query the data
+    result = conn.execute(
+        text(
+            dedent(
+                """
+                SELECT * FROM test
+                """,
+            ),
+        ),
+    )
+
+    for row in result:
+        print(row)
