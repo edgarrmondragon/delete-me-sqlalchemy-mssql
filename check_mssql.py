2608ad1a3bbec75b7e60a1d5b8e15fc8fac19744
Edgar Ramírez Mondragón <edgarrm358@gmail.com>
2023-07-31 16:18:59 -0600

    Use master db

--- check_mssql.py
+++ check_mssql.py
@@ -10,58 +10,58 @@ MSSQL_USER = os.environ["MSSQL_USER"]
 MSSQL_PASSWORD = os.environ["MSSQL_PASSWORD"]
 MSSQL_DB = os.environ["MSSQL_DB"]
 
-URI = f"mssql+pymssql://{MSSQL_USER}:{MSSQL_PASSWORD}@{MSSQL_ADDRESS}:{MSSQL_PORT}/{MSSQL_DB}"
+URI = f"mssql+pymssql://{MSSQL_USER}:{MSSQL_PASSWORD}@{MSSQL_ADDRESS}/master"
 
 engine = create_engine(URI)
 
 # Test the connection
 engine.execute(text("SELECT 1"))
 
-conn = engine.connect()
+# conn = engine.connect()
 
-print(f"Connected to {MSSQL_ADDRESS}:{MSSQL_PORT}")
+# print(f"Connected to {MSSQL_ADDRESS}:{MSSQL_PORT}")
 
-# Create the table
-conn.execute(
-    text(
-        dedent(
-            """
-            CREATE TABLE IF NOT EXISTS test (
-                id INT IDENTITY(1,1) PRIMARY KEY,
-                name VARCHAR(50) NOT NULL
-            )
-            """,
-        ),
-    ),
-)
+# # Create the table
+# conn.execute(
+#     text(
+#         dedent(
+#             """
+#             CREATE TABLE IF NOT EXISTS test (
+#                 id INT IDENTITY(1,1) PRIMARY KEY,
+#                 name VARCHAR(50) NOT NULL
+#             )
+#             """,
+#         ),
+#     ),
+# )
 
-print("Created table 'test'")
+# print("Created table 'test'")
 
-# Insert some data
-conn.execute(
-    text(
-        dedent(
-            """
-            INSERT INTO test (name) VALUES ('foo')
-            INSERT INTO test (name) VALUES ('bar')
-            INSERT INTO test (name) VALUES ('baz')
-            """,
-        ),
-    ),
-)
+# # Insert some data
+# conn.execute(
+#     text(
+#         dedent(
+#             """
+#             INSERT INTO test (name) VALUES ('foo')
+#             INSERT INTO test (name) VALUES ('bar')
+#             INSERT INTO test (name) VALUES ('baz')
+#             """,
+#         ),
+#     ),
+# )
 
-print("Inserted data into 'test'")
+# print("Inserted data into 'test'")
 
-# Query the data
-result = conn.execute(
-    text(
-        dedent(
-            """
-            SELECT * FROM test
-            """,
-        ),
-    ),
-)
+# # Query the data
+# result = conn.execute(
+#     text(
+#         dedent(
+#             """
+#             SELECT * FROM test
+#             """,
+#         ),
+#     ),
+# )
 
-for row in result:
-    print(row)
+# for row in result:
+#     print(row)
